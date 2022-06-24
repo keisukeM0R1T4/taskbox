e3fcda31396cfa4e4340cde550480b1bbfe227cd
keisukeM0R1T4 <keisuke.m0510@gmail.com>
2022-06-24 16:21:36 +0900

    first-commit:tutorial of storybook

---
+++ .storybook/preview.js
@@ -0,0 +1,17 @@
+import '../src/index.css';
+
+import { initialize, mswDecorator } from 'msw-storybook-addon';
+
+initialize();
+
+export const decorators = [mswDecorator];
+
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+};
---
+++ src/components/TaskList.js
@@ -0,0 +1,77 @@
+import React from 'react';
+import Task from './Task';
+import { useDispatch, useSelector } from 'react-redux';
+import { updateTaskState } from '../lib/store';
+
+export default function TaskList() {
+    const tasks = useSelector((state) => {
+        const tasksInOrder = [
+            ...state.taskbox.tasks.filter((t) => t.state === "TASK_PINNED"),
+            ...state.taskbox.tasks.filter((t) => t.state !== "TASK_PINNED"),
+        ];
+        const filteredTasks = tasksInOrder.filter(
+            (t) => t.state === 'TASK_INBOX' || t.state === 'TASK_PINNED'
+        );
+        return filteredTasks;
+    });
+
+    const { status } = useSelector((state) => state.taskbox);
+
+    const dispatch = useDispatch();
+
+    const pinTask = (value) => {
+        dispatch(updateTaskState({id:value, newTaskState: 'TASK_PINNED'}));
+    };
+
+    const archieveTask = (value) => {
+        dispatch(updateTaskState({id:value, newTaskState: 'TASK_ARCHIEVED'}));
+    };
+
+  const LoadingRow = (
+    <div className="loading-item">
+        <span className='glow-checkbox' />
+        <span className='grow-text'>
+            <span>Loading</span> <span>cool</span> <span>state</span>
+        </span>
+    </div>
+  );
+
+  if (status === 'loading' ) {
+    return (
+         <div className="list-items" data-testid="loading" key={"loading"}>
+            {LoadingRow}
+            {LoadingRow}
+            {LoadingRow}
+            {LoadingRow}
+            {LoadingRow}
+            {LoadingRow}
+        </div>
+    );
+  }
+
+  if (tasks.length === 0) {
+    return (
+        <div className="list-items" key={"empty"} data-testid="empty">
+          <div className="wrapper-message">
+            <span className="icon-check" />
+            <div className="title-message">You have no tasks</div>
+            <div className="subtitle-message">Sit back and relax</div>
+          </div>
+        </div>
+    );
+  }
+
+  return (
+    <div className="list-items" data-testid="success" key={"success"}>
+      {tasks.map((task) => (
+        <Task
+            key={task.id}
+            task={task}
+            onPinTask={(task) => pinTask(task)}
+            onArchiveTask={(task) => archieveTask(task)}
+        />
+      ))}
+    </div>
+  );
+}
+
